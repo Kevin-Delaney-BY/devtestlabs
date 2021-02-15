a2a674ce198df000b7445300756abd4f2027e075
Kevin-Delaney-BY <67275760+Kevin-Delaney-BY@users.noreply.github.com>
2021-02-15 10:44:25 +0000

    Create copy_from_share.ps1

---
+++ artifacts/greendale_file_share/copy_from_share.ps1
@@ -0,0 +1,23 @@
+$username = "greendale\adm_pat"
+$pass= 'F00tball'
+$password = ConvertTo-SecureString $pass -AsPlainText -Force
+$credentials = New-Object System.Management.Automation.PSCredential ($username,$password)
+$server = "10.10.10.5"
+$source = "\\10.10.10.5\greendaleshare\test_file.txt"
+$destination = "C:\WorkingFolder"
+
+if (!(Test-Path $destination)) {
+
+    New-Item -ItemType Directory -Path "C:\" -Name "WorkingFolder"
+
+   }
+
+
+
+
+New-PSDrive -Name P -PSProvider Microsoft.Powershell.core\Filesystem -Root "\\$server\greendaleshare" -Credential $credentials
+
+Copy-Item $source -Destination $destination
+
+
+Remove-PSDrive -Name P 
